542f606e639f3cd5daad73d9f91547cde5d18688
Steven She <shshe@uwaterloo.ca>
2010-12-17 16:44:43 -0500

    visibility condition statistic

--- src/test/scala/gsd/linux/KConfigTest.scala
+++ src/test/scala/gsd/linux/KConfigTest.scala
@@ -2,6 +2,7 @@ package gsd.linux
 
 import org.scalatest.junit.AssertionsForJUnit
 import org.junit.Test
+import stats.ASEStatistics
 
 class KConfigTest extends AssertionsForJUnit {
 
@@ -21,4 +22,63 @@ class KConfigTest extends AssertionsForJUnit {
     println(k)
   }
 
+  @Test
+  def removeInherited1 {
+    val in =
+      """
+      config IFUPDOWN_UDHCPC_CMD_OPTIONS string {
+       prompt "ifup udhcpc command line options" if [IFUPDOWN && UDHCPC]
+       default ["-R -n"] if [IFUPDOWN && UDHCPC]
+       depends on [IFUPDOWN && UDHCPC]
+       inherited [IFUPDOWN && UDHCPC]
+      }
+      """
+
+    val ck = ASEStatistics.removeInherited(KConfigParser.parseKConfig(in))
+    val ak = ck.toAbstractKConfig
+    expect(Yes)(ak.findConfig("IFUPDOWN_UDHCPC_CMD_OPTIONS").get.pro)
+    assert(ak.findConfig("IFUPDOWN_UDHCPC_CMD_OPTIONS").get.defs forall { _.cond == Yes})
+  }
+
+  @Test
+  def removeInherited2 {
+    val in =
+      """
+      config A tristate {
+       prompt "..." if [B && C && (D || E)]
+       inherited [B && C]
+
+       config X tristate {
+        prompt "..." if [A && B && C && D]
+        inherited [A && B && C]
+       }
+      }
+      """
+
+    val ck = ASEStatistics.removeInherited(KConfigParser.parseKConfig(in))
+    val ak = ck.toAbstractKConfig
+    expect(Id("D") || Id("E"))(ak.findConfig("A").get.pro)
+    expect(Id("D"))(ak.findConfig("X").get.pro)
+  }
+
+  @Test
+  def removeDependsOn1 {
+    val in =
+      """
+      config A tristate {
+       prompt "..." if [B && C && (D || E)]
+       depends on [B && C]
+
+       config X tristate {
+        prompt "..." if [A && B && C && D]
+        depends on [A && B && C]
+       }
+      }
+      """
+
+    val ck = ASEStatistics.removeDependsOn(KConfigParser.parseKConfig(in))
+    val ak = ck.toAbstractKConfig
+    expect(Id("D") || Id("E"))(ak.findConfig("A").get.pro)
+    expect(Id("D"))(ak.findConfig("X").get.pro)
+  }
 }
