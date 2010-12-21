b6de3a0f54103ea047b49580c3b2e986f8eb969a
Steven She <shshe@uwaterloo.ca>
2010-12-21 09:08:14 -0500

    update to ase statistics

--- src/main/scala/gsd/linux/stats/ASEStatistics.scala
+++ src/main/scala/gsd/linux/stats/ASEStatistics.scala
@@ -15,54 +15,43 @@ class ASEStatistics(ck: ConcreteKConfig) extends FeatureStatistics(ck) {
 
   lazy val apk = ppk.toAbstractKConfig
 
+
+  // Must have a prompt defined, but with a condition == 'yes'
+  lazy val configsWithNoVisConds = ppk.allConfigs filter { c =>
+    !c.prompt.isEmpty && (c.prompt forall { _.cond == Yes })
+  }
+
   // Explicitly defined prompt (i.e. condition not propagated by an inherited
   // or depends on condition)
   lazy val configsWithVisConds = ppk.allConfigs filter { c =>
     c.prompt exists { _.cond != Yes }
   }
 
-  lazy val configsWithDefaults = ppk.allConfigs filter { c =>
-    !c.defs.isEmpty
-  }
-
   // Configs with defaults that have the same condition as its prompt
-  lazy val configsWithTrueDefs = configsWithDefaults filter { c =>
+  lazy val configsWithTrueDefs = ppk.allConfigs filter { c =>
     c.defs exists { d => c.prompt exists { _.cond == d.cond } }
   } distinct
 
   // Sub-category: defaults with expressions that are not simple values
   // default 'y' acts like a computed default
-  lazy val configsWithTrueCompDefs = configsWithTrueDefs filter { c =>
+  lazy val configsWithTrueCompDefs = ppk.allConfigs filter { c =>
     c.defs exists { d => !d.iv.isInstanceOf[Value] || d.iv == Yes && d.cond != Yes }
   }
 
   // Sub-category: defaults with expressions that are simple values
-  lazy val configsWithTrueLitDefs = configsWithTrueDefs filter { c =>
+  lazy val configsWithTrueLitDefs = ppk.allConfigs filter { c =>
     c.defs exists { d => d.iv.isInstanceOf[Value] && (d.iv != Yes || d.cond == Yes) }
   }
 
   // Configs with defaults that have a condition different from its prompt
-  lazy val configsWithCondDerived = configsWithDefaults filter { c =>
+  lazy val configsWithCondDerived = ppk.allConfigs filter { c =>
     c.defs exists  { d => c.prompt exists { _.cond != d.cond } }
   } distinct
 
   // Configs that are NEVER user-selectable
   lazy val configsWithUncondDerived = ppk.allConfigs filter { c =>
-    c.prompt forall { _.cond == No }
+    c.prompt.isEmpty
   }
-
-  lazy val configsWithUncondDerivedAndDefs = configsWithUncondDerived filter { c =>
-    !c.defs.isEmpty
-  }
-
-  lazy val configsWithUncondDerivedAndNoDefs = configsWithUncondDerived filter { c =>
-    c.defs.isEmpty
-  }
-
-  lazy val configsWithUncondDerivedAndRevDeps  =
-    configsWithUncondDerived filter { c1 =>
-      !apk.configs.find { c2 => c2.id == c1.id }.get.rev.isEmpty
-    }
 
   lazy val configsWithRevDeps =
     apk.configs filter { !_.rev.isEmpty }
@@ -159,65 +148,62 @@ object ASEStatisticsMain {
 
   def dependencyStats(implicit stats: ASEStatistics) {
     // visibility conditions
-    println("Visibility Conditions: %4d / %4d".format(stats.configsWithVisConds.size, stats.allConfigs.size))
+    println("Vis. Cond.:          %4d / %4d".format(stats.configsWithVisConds.size, stats.allConfigs.size))
+    println("No Vis. Cond.:       %4d / %4d".format(stats.configsWithNoVisConds.size, stats.allConfigs.size))
+    // unconditionally derived
+    println("Uncond. Derived:     %4d / %4d".format(stats.configsWithUncondDerived.size, stats.allConfigs.size))
 
     println
 
     // TODO categories can overlap
-    println("With Defaults:         %4d / %4d".format(stats.configsWithDefaults.size, stats.allConfigs.size))
-
     // explicit defaults (soft constraints)
-    println("  True Defs:           %4d / %4d".format(stats.configsWithTrueDefs.size, stats.configsWithDefaults.size))
+    println("Real Defs:           %4d / %4d".format(stats.configsWithTrueDefs.size, stats.allConfigs.size))
 
     //    computed (expressions)
-    println("    Computed:          %4d / %4d".format(stats.configsWithTrueCompDefs.size, stats.configsWithTrueDefs.size))
+    println("  Computed:          %4d / %4d".format(stats.configsWithTrueCompDefs.size, stats.configsWithTrueDefs.size))
 
     //    literals
-    println("    Literals:          %4d / %4d".format(stats.configsWithTrueLitDefs.size, stats.configsWithTrueDefs.size))
+    println("  Literals:          %4d / %4d".format(stats.configsWithTrueLitDefs.size, stats.configsWithTrueDefs.size))
 
     //    conditionally derived
-    println("  Cond. Derived:       %4d / %4d".format(stats.configsWithCondDerived.size, stats.configsWithDefaults.size))
-
-
-    // unconditionally derived
-    println("Uncond. Derived:       %4d / %4d".format(stats.configsWithUncondDerived.size, stats.configsWithDefaults.size))
-    println("  With Default:        %4d / %4d".format(stats.configsWithUncondDerivedAndDefs.size, stats.configsWithUncondDerived.size))
-    println("  With No Default:     %4d / %4d".format(stats.configsWithUncondDerivedAndNoDefs.size, stats.configsWithUncondDerived.size))
-    println("  With Rev Deps:       %4d / %4d".format(stats.configsWithUncondDerivedAndRevDeps.size, stats.configsWithUncondDerived.size))
+    println("Cond. Derived:       %4d / %4d".format(stats.configsWithCondDerived.size, stats.allConfigs.size))
 
     println
     // Reverse dependencies
-    println("Reverse Dependencies:  %4d / %4d".format(stats.configsWithRevDeps.size, stats.allConfigs.size))
+    println("Reverse Deps:        %4d / %4d".format(stats.configsWithRevDeps.size, stats.allConfigs.size))
 
   }
 
   def intersectionStats(implicit stats: ASEStatistics) {
     import Intersection._
 
-    val res =
-      calculatePairWise(Array((stats.configsWithUncondDerived map { _.id }).toSet,
-                      (stats.configsWithDefaults map { _.id }).toSet,
-                      (stats.configsWithRevDeps map { _.id }).toSet))
-
-    printPairWiseSizes(res, Array("Uncond. Derived", "w/Defaults", "w/Rev Deps"))
-
-    println
+//    val res =
+//      calculatePairWise(Array((stats.configsWithUncondDerived map { _.id }).toSet,
+//                      (stats.configsWithTrueDefs map { _.id }).toSet,
+//                      (stats.configsWithRevDeps map { _.id }).toSet))
+//
+//    printPairWiseSizes(res, Array("Uncond.Derived", "w/RealDefaults", "w/RevDeps"))
+//
+//    println
 
     val combos =
       calculatePartitions(
         Array((stats.configsWithUncondDerived map { _.id }).toSet,
               (stats.configsWithCondDerived map { _.id }).toSet,
               (stats.configsWithTrueDefs map { _.id }).toSet),
-        Array((stats.configsWithDefaults map { _.id }).toSet,
+        Array((stats.configsWithTrueDefs map { _.id }).toSet,
               (stats.configsWithRevDeps map { _.id }).toSet),
-        Array("w/Defaults", "w/RevDep"))
+        Array("w/RealDefaults", "w/RevDep"))
 
-    printComboSizes(combos, Array("Uncond.Derived", "Cond.Derived", "True Def."))
+    printComboSizes(combos, Array("Uncond.Derived", "Cond.Derived", "RealDefs."))
   }
 
   def main(args: Array[String]) {
     println("Parsing exconfig...")
-    val k = KConfigParser.parseKConfigFile("/home/shshe/Dev/extracts/2.6.32.exconfig")
+    val k = KConfigParser.parseKConfigFile(
+      "/home/shshe/Dev/thorsten/DependencyMiningPaperTools/input/toybox.exconfig"
+    )
+    //val k = KConfigParser.parseKConfigFile("/home/shshe/Dev/extracts/2.6.32.exconfig")
     implicit val stats = new ASEStatistics(k)
 
 //    println
