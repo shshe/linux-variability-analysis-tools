3e280463679c2757f0b30c51e893acf82a5e4341
Steven She <shshe@uwaterloo.ca>
2012-02-11 21:46:24 -0500

    incremental analysis sub-project

--- project/Build.scala
+++ project/Build.scala
@@ -5,6 +5,7 @@ object LVATBuild extends Build {
     lazy val root = Project(id = "lvat", base = file("."))
     lazy val fmTranslation = Project(id = "fm-translation", base = file("fm-translation/")) dependsOn (root)
     lazy val paperTools = Project(id = "paper-tools", base = file("paper-tools/")) dependsOn (fmTranslation)
+    lazy val incremental = Project(id = "incremental", base = file("incremental/")) dependsOn (fmTranslation)
 }
 
 // vim: set ts=4 sw=4 et:
